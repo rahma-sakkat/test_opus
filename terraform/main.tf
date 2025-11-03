a277f43422e785f26221465509d65ac6f74d30ef
rahma-sakkat <158100203+rahma-sakkat@users.noreply.github.com>
2025-11-03 21:51:40 +0100

    fix6

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,20 +11,9 @@ resource "azurerm_mysql_flexible_server" "mysql" {
   name                = "testopus-mysql"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  version             = "8.0"
+  version             = "8.0.21"
   administrator_login = "adminuser"
   administrator_password = "SuperSecret123!"
   sku_name            = "B_Standard_B1ms"
-
-  # Optional: minimal storage size directly in GB
-  #storage_mb = 5120      # 5 GB in MB
-
-  # Optional HA
-  #high_availability_mode = "Disabled"
-
-  # Public access
-  public_network_access_enabled = true
-
-  # Backup retention
   backup_retention_days = 7
 }
