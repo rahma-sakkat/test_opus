bb98b7ea9efbdc297fc2350fef8a71b25c36237c
rahma-sakkat <158100203+rahma-sakkat@users.noreply.github.com>
2025-11-07 19:39:44 +0100

    final_touches

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,9 @@
 provider "azurerm" {
   features {}
- 
+
+  subscription_id = "613d8db0-49cd-47a5-af2f-4956bf6a8d5d"  # your subscription
+  # optional but recommended if you know them:
+  tenant_id       = "d5b5bfcf-d099-4fde-981f-0f0618bccdec"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -9,12 +12,12 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_mysql_flexible_server" "mysql" {
-  name                = "testopus-mysql"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  version             = "8.0.21"
-  administrator_login = "adminuser"
+  name                   = "testopus-mysql"
+  resource_group_name    = azurerm_resource_group.rg.name
+  location               = azurerm_resource_group.rg.location
+  version                = "8.0.21"
+  administrator_login    = "adminuser"
   administrator_password = "SuperSecret123!"
-  sku_name            = "B_Standard_B1ms"
-  backup_retention_days = 7
+  sku_name               = "B_Standard_B1ms"
+  backup_retention_days  = 7
 }
